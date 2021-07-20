6bc4295bbc242c0817b1ff5364e2172e0590c7cc
sopspops <adorable_sj@hotmail.com>
2021-07-20 10:32:18 -0700

    added async/await to mongoose.connect

--- server.js
+++ server.js
@@ -6,36 +6,30 @@ const methodOverride = require('method-override')
 const port = process.env.PORT || 3000
 const app = express()
 
-
-async function main() {await mongoose.connect('mongodb://localhost/blog', { 
+const connectDB = async () => {try {await mongoose.connect('mongodb://localhost/blog', { 
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, keepAlive : true,
     bufferMaxEntries: 0, useFindAndModify: false 
-}).then(connection => {
-    console.log('Connected to MongoDB DB')
-  })
-  .catch(error => {
-  console.log(error.message)
-  })
+})
+    console.log('MongoDB connected!!');
+} catch (err) {
+    console.log('Failed to connect to MongoDB', err);
+}}
 
-  app.set('view engine', 'ejs')
-  app.use(express.urlencoded({ extended: false }))
-  app.use(methodOverride('_method'))
-  
-  app.get('/', async (req, res) => {
-      const articles = await Article.find().sort({
-          createdAt: 'desc'
-      })
-      res.render('./articles/index', { articles: articles })
-  })
-  
-  app.use('/articles', articleRouter)
-  
-  app.listen(port)
-  }
+connectDB()
 
 
-  main()
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({
+        createdAt: 'desc'
+    })
+    res.render('./articles/index', { articles: articles })
+})
 
+app.use('/articles', articleRouter)
 
+app.listen(port)
 
