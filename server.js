5befaca6f2b2491d0944d66a572427de0a5e804b
sopspops <adorable_sj@hotmail.com>
2021-07-20 09:49:10 -0700

    trying this and that..

--- server.js
+++ server.js
@@ -6,28 +6,57 @@ const methodOverride = require('method-override')
 const port = process.env.PORT || 3000
 const app = express()
 
-mongoose.connect('mongodb://localhost/blog', { 
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, keepAlive : true,
-    bufferMaxEntries: 0 
-}).then(connection => {
-    console.log('Connected to MongoDB DB')
-  })
-  .catch(error => {
-  console.log(error.message)
-  })
+async function main () {
+    await mongoose.connect('mongodb://localhost/blog', { 
+        useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, keepAlive : true,
+        bufferMaxEntries: 0 
+    }).then(connection => {
+        console.log('Connected to MongoDB DB')
+      })
+      .catch(error => {
+      console.log(error.message)
+      })
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
+      app.set('view engine', 'ejs')
+      app.use(express.urlencoded({ extended: false }))
+      app.use(methodOverride('_method'))
+      
+      app.get('/', async (req, res) => {
+          const articles = await Article.find().sort({
+              createdAt: 'desc'
+          })
+          res.render('./articles/index', { articles: articles })
+      })
+      
+      app.use('/articles', articleRouter)
+      
+      app.listen(port)
+}
 
-app.get('/', async (req, res) => {
-    const articles = await Article.find().sort({
-        createdAt: 'desc'
-    })
-    res.render('./articles/index', { articles: articles })
-})
+main();
 
-app.use('/articles', articleRouter)
+// mongoose.connect('mongodb://localhost/blog', { 
+//     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, keepAlive : true,
+//     bufferMaxEntries: 0 
+// }).then(connection => {
+//     console.log('Connected to MongoDB DB')
+//   })
+//   .catch(error => {
+//   console.log(error.message)
+//   })
 
-app.listen(port)
+// app.set('view engine', 'ejs')
+// app.use(express.urlencoded({ extended: false }))
+// app.use(methodOverride('_method'))
+
+// app.get('/', async (req, res) => {
+//     const articles = await Article.find().sort({
+//         createdAt: 'desc'
+//     })
+//     res.render('./articles/index', { articles: articles })
+// })
+
+// app.use('/articles', articleRouter)
+
+// app.listen(port)
 
